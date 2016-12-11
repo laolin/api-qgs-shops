294027bfba8970cfcb3118fe83c89a287908e831
LaoLin <hi@laolin.com>
2016-12-11 18:52:00 +0800

    允许用：空格，逗号，分号分隔多个关键字

--- src/api_foot.php
+++ src/api_foot.php
@@ -15,16 +15,22 @@ class class_foot{
     public static function search( $para1,$para2) {
       $res=API::msg(0,'Ok.');
       $db=API::db();
-      $s=API::GET('s');
+      $keys=API::GET('s');
+      
+      $where=["LIMIT" => 100];
+      if(strlen($keys)==0) {
+      } else {
+        $k= preg_split("/[\s,;]+/",$keys);
+        for($i=count($k); $i--;  ) {
+          $w_or["or#".$i]=[ 'tel[~]'=>$k[$i], 'addr[~]'=>$k[$i], 'name[~]'=>$k[$i] ];
+        }
+        $where=['and' => $w_or, "LIMIT" => 100 ];
+        var_dump($where);
+      }
       //id	dpid	name	region	region_name	addr	tel	stars	star_count	scores	score_count	lnglat	mark1	mark2
       $r=$db->select('dp01_shop_v3',
           ['id','dpid','name','region_name','addr','tel','stars','star_count','scores','score_count','lnglat' ],
-          ["or" => [
-                      'tel[~]'=>$s,
-                      'addr[~]'=>$s,
-                      'name[~]'=>$s
-                   ] ,
-            "LIMIT" => 100  ]);
+          $where);
       $res['data']=$r;
       return $res;
     }
